3362509078a2f54ee4e04c1ff2cf5ceb4a3ab80e
ParzivalFR <parzivaleu@gmail.com>
2024-04-07 18:48:48 +0200

    Fix Top component

--- src/components/Top/Top.jsx
+++ src/components/Top/Top.jsx
@@ -1,15 +1,25 @@
+import { useState } from "react";
 import "./top.scss";
 
 const ScrollToTopButton = () => {
+  const [clicked, setClicked] = useState(false);
+
   const scrollToTop = () => {
     window.scrollTo({
       top: 0,
       behavior: "smooth",
     });
+    setClicked(true); // Définir clicked à true lors du clic
+    setTimeout(() => {
+      setClicked(false); // Réinitialiser clicked après un court délai
+    }, 1000); // Ajustez le délai
   };
 
   return (
-    <button className="scroll-to-top" onClick={scrollToTop}>
+    <button
+      className={`scroll-to-top ${clicked ? "clicked" : ""}`}
+      onClick={scrollToTop}
+    >
       <i className="fa-solid fa-arrow-up"></i>
     </button>
   );
